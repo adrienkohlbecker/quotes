c517a2eac177652db833f085acf6c88995eb2f73
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2021-01-06 15:56:28 +0100

    Fix eres

--- scrapers/eres.rb
+++ scrapers/eres.rb
@@ -7,18 +7,18 @@ require 'active_support/time'
 module Scrapers
   class Eres
     def initialize(id)
-      uri = "https://www.eres-group.com/eres/new_fiche_export.php?id=#{id}&format=CSV"
+      uri = "https://www.eres-group.com/eres/new_fiche_json.php?id=#{id}"
       options = {
         headers: { 'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.43 Safari/537.36' }
       }
-      @csv = CSV.parse(HTTParty.get(uri, options).body, col_sep: ';')
+      @json = JSON.parse(HTTParty.get(uri, options).body)
     end
 
     def quotation_history
-      @csv.reverse.map do |r|
+      @json.reverse.map do |r|
         {
-          date: r[2],
-          close: r[3].to_f
+          date: r["d"],
+          close: r["p"]
         }
       end
     end
