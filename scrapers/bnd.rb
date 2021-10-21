ecc2538003defaf6b621e8f9fc1ff53ae29fb7c3
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2021-10-21 10:40:29 +0200

    Fix BND

--- scrapers/bnd.rb
+++ scrapers/bnd.rb
@@ -7,32 +7,21 @@ require 'active_support/time'
 module Scrapers
   class BND
     def initialize(fund_id)
-      uri = 'https://secure.brandnewday.nl/service/navvaluesforfund'
-      body = {
-        'sort' => '',
-        'page' => 1,
-        'pageSize' => 1000,
-        'group' => '',
-        'filter' => '',
-        'fundId' => fund_id,
-        'startDate' => '01-01-2010',
-        'endDate' => Date.today.strftime('%d-%m-%Y')
-      }
+      uri = "https://devrobotapi.azurewebsites.net/roboadvisor/v1/fundrates?id=#{fund_id}"
       options = {
         headers: { 'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.43 Safari/537.36' },
-        body: body
       }
-      @json = JSON.parse(HTTParty.post(uri, options).body)
+      @json = JSON.parse(HTTParty.get(uri, options).body)
     end
 
     def quotation_history
-      raise 'weird data' if @json['Total'] < 50 || !@json['Errors'].nil?
+      raise 'weird data' if @json['rates'].length < 50 || !@json['status'].nil?
 
-      @json['Data'].map do |item|
-        date = Time.at(item['RateDate'].match(/.*\((\d+)\).*/)[1].to_i / 1000).to_date
+      @json['rates'].map do |item|
+        date = Time.parse(item['date']).to_date
         {
           date: date.strftime('%Y-%m-%d'),
-          close: item['LastRate']
+          close: item['nav']
         }
       end
     end
